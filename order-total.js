8b394d8285c7f0fb218222811aa014aa39c3842d
Vikash S <vikash.shaw@imerit.net>
2018-12-15 12:48:07 +0530

    promise added in fn

--- order-total.js
+++ order-total.js
@@ -1,6 +1,6 @@
 const orderTotal = (order) => {
-    return order.items.reduce((prev, cur) => 
-    cur.price * (cur.quantity || 1) + prev, 0);
+    return Promise.resolve(order.items.reduce((prev, cur) => 
+    cur.price * (cur.quantity || 1) + prev, 0));
 }
 
 module.exports = orderTotal;
--- order-total.test.js
+++ order-total.test.js
@@ -8,20 +8,33 @@ const orderTotal = require('./order-total');
 
 it('Quantity', () => {
 
-    expect(orderTotal({
+/*     expect(orderTotal({
             items:[
                 {name: 'A', price: 3, quantity: 2}
             ]
-        })).toBe(6);
+        })).toBe(6); */
+
+      orderTotal({
+        items:[
+            {name: 'A', price: 3, quantity: 2}
+        ]
+    }).then(result => expect(result).toBe(6)); 
 });
 
 
 it('No Quantity', () => {
 
-    expect(orderTotal({
+    /* expect(orderTotal({
             items:[
                 {name: 'A', price: 50},
                 {name: 'B', price: 40}
             ]
-        })).toBe(90);
+        })).toBe(90); */
+     
+    orderTotal({
+        items:[
+            {name: 'A', price: 50},
+            {name: 'B', price: 40}
+        ]
+    }).then(result => expect(result).toBe(90));
 });
